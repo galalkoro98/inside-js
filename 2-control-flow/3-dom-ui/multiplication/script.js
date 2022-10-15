a9dd99042c93227bcd3358ceb118511d15e0ee0f
Galalkoro98 <galal.koro@gmail.com>
2022-10-15 21:21:13 +0200

    exercise done

--- 2-control-flow/3-dom-ui/multiplication/script.js
+++ 2-control-flow/3-dom-ui/multiplication/script.js
@@ -1,11 +1,23 @@
 import { readNumber, display } from '../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById('do-math').addEventListener('click', () => {
   debugger;
 
   // read user values
 
+  let userV1 =  readNumber('left');
+  let userV2 = readNumber('right');
+  let userM = 0;
+
   // use a for loop to multiply the two numbers
+  for(let i = userV1; i <= userV2; i++){
+    userM = userV1 * userV2;
+    userV1++;
+    
+  }
+ 
 
   // display the product
+
+  display('product', userM);
 });
