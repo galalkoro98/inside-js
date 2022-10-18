9b3627394648ca1f7892592fa04604a758320625
Galalkoro98 <galal.koro@gmail.com>
2022-10-18 21:54:09 +0200

    exercise done

--- 2-control-flow/3-dom-ui/repeat-characters/script.js
+++ 2-control-flow/3-dom-ui/repeat-characters/script.js
@@ -1,15 +1,25 @@
 import { readNumber, readString, display } from '../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById('repeat-them').addEventListener('click', () => {
   debugger;
 
   // read user values
+  let chaN = readNumber('number-of-times');
+  let chaS = readString('user-text');
+  let repeatEachCha = 0;
 
   // repeat the characters in the text
 
-  for (_; _; _) {
-    for (_; _; _) {}
+  for (let i = chaN; i <= chaS; i++) {
+    for (let j = chaS; j > i; j++) {
+      repeatEachCha += readString[j]
+    
+    }
+
+    readString += readNumber[i];
   }
 
   // display the text with repeated characters
+
+display('repeated-output', repeatEachCha)
 });
